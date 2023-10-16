65ee6311649acbef9b8502db452cabe95684b4f4
EAST-J <jsj630@zju.edu.cn>
2023-10-16 16:07:04 +0800

    update

---
+++ pubs.js
@@ -0,0 +1,470 @@
+// Modify the `PUBLICATIONS` object
+
+const PUBLICATIONS = [
+  {
+    title:
+      "Seal-3D: Interactive Pixel-Level Editing for Neural Radiance Fields",
+    authors:
+      "Xiangyu Wang, Jingsen Zhu, Qi Ye, Yuchi Huo, Yunlong Ran, Zhihua Zhong, Jiming Chen",
+    image: "http://www.cad.zju.edu.cn/home/huo/images/pubs/iccv23-seal3d.png",
+    conference: "ICCV 2023",
+    links: {
+      "project page": "https://windingwind.github.io/seal-3d/",
+      arxiv: "https://arxiv.org/abs/2307.15131",
+      pdf: "https://arxiv.org/pdf/2307.15131",
+      supp: "",
+      video: "https://windingwind.github.io/seal-3d/static/videos/supp.mp4",
+      code: "https://github.com/windingwind/seal-3d/",
+    },
+    badges: ["CCF A"],
+  },
+  {
+    title:
+      "F&F Attack: Adversarial Attack against Multiple Object Trackers by Inducing False Negatives and False Positives",
+    authors:
+      "Tao Zhou, Wenhan Luo, Qi Ye, Kaihao Zhang, Zhiguo Shi, Jiming Chen",
+    image: "https://infzhou.github.io/FnFAttack/resources/Main_Figure.png",
+    conference: "ICCV 2023",
+    links: {
+      "project page": "https://infzhou.github.io/FnFAttack/index.html",
+      arxiv: "",
+      pdf: "",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: ["CCF A"],
+  },
+  {
+    title:
+      "3D Grasp Synthesis via Hand-Object Contact Constraint. International Joint Conferences on Artificial Intelligence",
+    authors:
+      "Haoming Li, Xinzhuo Lin, Yang Zhou, Xiang Li, Yuchi Huo, Jiming Chen, Qi Ye",
+    image: "",
+    conference: "IJCAI 2023",
+    links: {
+      "project page": "",
+      arxiv: "",
+      pdf: "",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "InterTracker: Discovering and Tracking General Objects Interacting with Hands in the Wild",
+    authors: "Yanyan Shao, Qi Ye, Wenhan Luo, Kaihao Zhang, Jiming Chen",
+    image: "",
+    conference: "IROS 2023",
+    links: {
+      "project page": "",
+      arxiv: "http://arxiv.org/abs/2308.03061",
+      pdf: "http://arxiv.org/pdf/2308.03061",
+      supp: "",
+      video: "https://youtu.be/cDTl51qLmLg",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "DexRepNet: Learning Dexterous Robotic Grasping Network with Geometric and Spatial Hand-Object Representation",
+    authors:
+      "Qingtao Liu, Yu Cui, Qi Ye, Zhengnan Sun, Haoming Li, Gaofeng Li, Lin Shao, Jiming Chen",
+    image: "",
+    conference: "IROS 2023",
+    links: {
+      "project page": "",
+      arxiv: "https://arxiv.org/abs/2303.09806",
+      pdf: "https://arxiv.org/pdf/2303.09806",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "I2-SDF: Intrinsic Indoor Scene Reconstruction and Editing via Raytracing in Neural SDFs",
+    authors:
+      "Jingsen Zhu, Yuchi Huo, Qi Ye,Fujun Luan, Jifan Li, Dianbing Xi, Lisha Wang, Rui Tang, Wei Hua, Hujun Bao, Rui Wang",
+    image: "http://www.cad.zju.edu.cn/home/huo/images/pubs/23iisdf.jpg",
+    conference: "CVPR 2023",
+    links: {
+      "project page": "https://jingsenzhu.github.io/i2-sdf/",
+      paper:
+        "https://openaccess.thecvf.com/content/CVPR2023/html/Zhu_I2-SDF_Intrinsic_Indoor_Scene_Reconstruction_and_Editing_via_Raytracing_in_CVPR_2023_paper.html",
+      arxiv: "https://arxiv.org/abs/2303.07634",
+      pdf: "https://openaccess.thecvf.com/content/CVPR2023/papers/Zhu_I2-SDF_Intrinsic_Indoor_Scene_Reconstruction_and_Editing_via_Raytracing_in_CVPR_2023_paper.pdf",
+      supp: "https://openaccess.thecvf.com/content/CVPR2023/supplemental/Zhu_I2-SDF_Intrinsic_Indoor_CVPR_2023_supplemental.pdf",
+      video: "",
+      code: "https://github.com/jingsenzhu/i2-sdf",
+      data: "https://mega.nz/folder/jdhDnTqL#Ija678SU2Va_JJOiwqmdEg",
+    },
+    badges: [],
+  },
+  {
+    title: "Metadata-Based RAW Reconstruction via Implicit Neural Functions",
+    authors: "Leyi Li, Huijie Qiao, Qi Ye, Qinmin Yang",
+    image: "",
+    conference: "CVPR 2023",
+    links: {
+      "project page": "",
+      paper:
+        "https://openaccess.thecvf.com/content/CVPR2023/html/Li_Metadata-Based_RAW_Reconstruction_via_Implicit_Neural_Functions_CVPR_2023_paper.html",
+      pdf: "https://openaccess.thecvf.com/content/CVPR2023/papers/Li_Metadata-Based_RAW_Reconstruction_via_Implicit_Neural_Functions_CVPR_2023_paper.pdf",
+      arxiv: "",
+      supp: "https://openaccess.thecvf.com/content/CVPR2023/supplemental/Li_Metadata-Based_RAW_Reconstruction_CVPR_2023_supplemental.pdf",
+      video: "https://www.youtube.com/watch?v=vNLRd-iy1X0",
+      code: "https://github.com/lly-louis/INF",
+      "colab demo":
+        "https://colab.research.google.com/drive/1KFNFQgLcQ7HwIFn7fMFh4DiwZ0tb4hyv?usp=sharing",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "Implementation and Optimization of Grasping Learning with Dual-modal Soft Gripper.",
+    authors:
+      "Lei Zhao ,Haoyue Liu ,Feihan Li ,Xingyu Ding ,Yuhao Sun ,Fuchun Sun , Jianhua Shan ,Qi Ye, Lincheng Li , Bin Fang",
+    image: "",
+    conference: "ICRA 2023",
+    links: {
+      "project page": "",
+      paper: "https://ieeexplore.ieee.org/abstract/document/10161249",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "NeurAR: Neural Uncertainty for Autonomous 3D Reconstruction with Implicit Neural Representations",
+    authors:
+      "Yunlong Ran, Jing Zeng, Shibo He, Lincheng Li, Yingfeng Chen, Gimhee Lee, Jiming Chen, Qi Ye",
+    image: "",
+    conference: "ICRA 2023",
+    links: {
+      "project page": "",
+      arxiv: "https://arxiv.org/abs/2207.10985",
+      pdf: "https://arxiv.org/pdf/2207.10985",
+      supp: "",
+      video: "",
+      code: "https://github.com/KingteeLoki-Ran/NeurAR",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "Efficient View Path Planning for Autonomous Implicit Reconstruction",
+    authors:
+      "Jing Zeng, Yanxu Li, Yunlong Ran, Shuo Li, Fei Gao, Lincheng Li, Shibo He, Jiming chen, Qi Ye",
+    image: "",
+    conference: "ICRA 2023",
+    links: {
+      "project page": "",
+      paper: "https://ieeexplore.ieee.org/document/10160793",
+      arxiv: "https://arxiv.org/abs/2209.13159",
+      pdf: "https://arxiv.org/pdf/2209.13159",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "ImmFusion: Robust mmWave-RGB Fusion for 3D Human Body Reconstruction in All Weather Conditions",
+    authors:
+      "Anjun Chen, Xiangyu Wang, Kun Shi, Shaohao Zhu, Bin Fang, Yingfeng Chen, Jiming Chen, Yuchi Huo, Qi Ye",
+    image: "https://chen3110.github.io/ImmFusion/resources/framework.png",
+    conference: "ICRA 2023",
+    links: {
+      "project page": "https://chen3110.github.io/ImmFusion/index.html",
+      paper: "",
+      arxiv: "https://arxiv.org/abs/2210.01346",
+      pdf: "https://arxiv.org/pdf/2210.01346",
+      supp: "",
+      video: "",
+      code: "https://github.com/Chen3110/ImmFusion",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "mmBody Benchmark: 3D Body Reconstruction Dataset and Analysis for Millimeter Wave Radar",
+    authors:
+      "Anjun Chen, Xiangyu Wang, Shaohao Zhu, Yanxu Li, Jiming Chen, Qi Ye",
+    image: "https://chen3110.github.io/mmbody/resources/good_case.png",
+    conference: "ACM MM 2022",
+    links: {
+      "project page": "https://chen3110.github.io/mmbody/",
+      paper: "https://dl.acm.org/doi/10.1145/3503161.3548262",
+      arxiv: "https://arxiv.org/abs/2209.05070",
+      pdf: "https://arxiv.org/pdf/2209.05070",
+      supp: "",
+      video: "",
+      code: "",
+      data: "https://github.com/Chen3110/mmBody",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "APPTracker: Improving Tracking Multiple Objects in Low-Frame-Rate Videos",
+    authors: "Tao Zhou, Wenhan Luo, Zhiguo Shi, Jiming Chen, Qi Ye",
+    image: "https://infzhou.github.io/appTracker/resources/Fig2_pipeline.png",
+    conference: "ACM MM 2022",
+    links: {
+      "project page": "https://infzhou.github.io/appTracker/",
+      paper:
+        "https://infzhou.github.io/folder/Zhou_APPTracker_Improving_Tracking_Multiple_Objects_in_Low-Frame-Rate_Videos_MM_2022.pdf",
+      arxiv: "",
+      pdf: "",
+      supp: "",
+      video: "https://youtu.be/Rzbda9vJofs",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "Geometry-based Distance Decomposition for Monocular 3D Object Detection",
+    authors:
+      "Xuepeng Shi, Qi Ye, Xiaozhi Chen, Chuangrong Chen, Zhixiang Chen, Tae-Kyun Kim",
+    image: "",
+    conference: "ICCV 2021",
+    links: {
+      "project page": "",
+      paper: "https://ieeexplore.ieee.org/document/9711219",
+      arxiv: "https://arxiv.org/abs/2104.03775",
+      pdf: "https://arxiv.org/pdf/2104.03775",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "The Phong Surface: Efficient 3D Model Fitting using Lifted Optimization",
+    authors:
+      "Jingjing Shen, Thomas J. Cashman, Qi Ye, Tim Hutton, Toby Sharp, Federica Bogo, Andrew William Fitzgibbon, Jamie Shotton",
+    image: "",
+    conference: "ECCV 2020",
+    links: {
+      "project page": "",
+      paper: "",
+      arxiv: "https://arxiv.org/abs/2007.04940",
+      pdf: "https://arxiv.org/pdf/2007.04940",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "Occlusion-aware hand pose estimation using hierarchical mixture density network",
+    authors: "Qi Ye, Tae-Kyun Kim",
+    image: "",
+    conference: "ECCV 2018",
+    links: {
+      "project page": "",
+      paper: "",
+      arxiv: "https://arxiv.org/abs/1711.10872",
+      pdf: "https://arxiv.org/pdf/1711.10872",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "Opening the black box: Hierarchical sampling optimization for hand pose estimation",
+    authors:
+      "Danhang Tang; Qi Ye; Shanxin Yuan; Jonathan Taylor; Pushmeet Kohli; Cem Keskin; Tae-Kyun Kim; Jamie Shotton",
+    image: "",
+    conference:
+      "IEEE transactions on pattern analysis and machine intelligence 2018",
+    links: {
+      "project page": "",
+      paper: "https://ieeexplore.ieee.org/document/8386667",
+      arxiv: "",
+      pdf: "",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "BigHand2.2M Benchmark: Hand Pose Dataset and State of the Art Analysis",
+    authors: "Shanxin Yuan, Qi Ye, Bjorn Stenger, Siddhant Jain, Tae-Kyun Kim",
+    image: "",
+    conference: "CVPR 2017",
+    links: {
+      "project page": "",
+      paper:
+        "https://openaccess.thecvf.com/content_cvpr_2017/html/Yuan_BigHand2.2M_Benchmark_Hand_CVPR_2017_paper.html",
+      arxiv: "",
+      pdf: "",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title:
+      "Spatial Attention Deep Net with Partial PSO for Hierarchical Hybrid Hand Pose Estimation",
+    authors: "Qi Ye, Shanxin Yuan, Tae-Kyun Kim",
+    image: "",
+    conference: "ECCV 2016",
+    links: {
+      "project page": "",
+      paper: "",
+      arxiv: "https://arxiv.org/abs/1604.03334",
+      pdf: "https://arxiv.org/pdf/1604.03334",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+  {
+    title: "The robustness of a new 3D CAPTCHA",
+    authors: "Qi Ye, Youbin Chen, Bin Zhu",
+    image: "",
+    conference:
+      "2014 11th IAPR International Workshop on Document Analysis Systems",
+    links: {
+      "project page": "",
+      paper: "",
+      arxiv: "https://arxiv.org/abs/1604.03334",
+      pdf: "https://arxiv.org/pdf/1604.03334",
+      supp: "",
+      video: "",
+      code: "",
+    },
+    badges: [],
+  },
+];
+
+const HIGHLIGHT_NAME = "qi ye";
+
+// Do not touch the code below
+
+// Convert PUBLICATIONS to HTML table
+document.addEventListener("DOMContentLoaded", (ev) => {
+  const tbody = document.createElement("tbody");
+  const rows = PUBLICATIONS.map((data) => {
+    const row = document.createElement("tr");
+    row.append(createImageCell(data.image), createContentCell(data));
+    return row;
+  });
+  tbody.append(...rows);
+  document.querySelector("#pub-list").append(tbody);
+});
+
+// Functions to create HTML elements
+function createImageCell(link) {
+  const cell = document.createElement("td");
+  cell.style.padding = "10px";
+  cell.style.width = "30%";
+  cell.style.verticalAlign = "middle";
+  const img = document.createElement("img");
+  img.src = link;
+  img.style.width = "100%";
+  cell.append(img);
+  return cell;
+}
+
+function createContentCell({ title, authors, conference, links, badges }) {
+  const cell = document.createElement("td");
+  cell.style.padding = "20px";
+  cell.style.width = "70%";
+  cell.style.verticalAlign = "middle";
+  const titleElem = document.createElement("papertitle");
+  titleElem.textContent = title;
+  const authorsElem = createAuthorElement(authors);
+  const conferenceElem = document.createElement("em");
+  conferenceElem.textContent = conference;
+  const badgesElem = createBadgesElement(badges);
+  const linksElem = createLinksElement(links);
+  const elements = [
+    titleElem,
+    document.createElement("br"),
+    authorsElem,
+    document.createElement("br"),
+    conferenceElem,
+    badgesElem,
+    document.createElement("br"),
+    linksElem,
+  ].filter((elem) => elem);
+  cell.append(...elements);
+  return cell;
+}
+
+function createAuthorElement(authors) {
+  if (!authors) {
+    return null;
+  }
+  const frag = document.createDocumentFragment();
+  const elements = authors.split(/[,;]/g).map((author) => {
+    const authorText = author.trim();
+    if (authorText.toLowerCase() === HIGHLIGHT_NAME) {
+      const authorElem = document.createElement("strong");
+      authorElem.textContent = authorText;
+      return authorElem;
+    }
+    return authorText;
+  });
+  for (let i = 0; i < elements.length; i++) {
+    frag.append(elements[i]);
+    if (i !== elements.length - 1) {
+      frag.append(", ");
+    }
+  }
+  return frag;
+}
+
+function createBadgesElement(badges) {
+  if (!badges) {
+    return null;
+  }
+  const frag = document.createDocumentFragment();
+  const textElem = document.createElement("span");
+  textElem.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;";
+  frag.append(textElem);
+  frag.append(
+    ...badges.map((badge) => {
+      const badgeElem = document.createElement("span");
+      badgeElem.classList.add("badge");
+      badgeElem.textContent = badge;
+      return badgeElem;
+    })
+  );
+  return frag;
+}
+
+function createLinksElement(links) {
+  if (!links) {
+    return null;
+  }
+  const frag = document.createDocumentFragment();
+  for (const [name, link] of Object.entries(links)) {
+    if (!link) {
+      continue;
+    }
+    const elem = document.createElement("a");
+    elem.href = link;
+    elem.textContent = "[" + name + "]";
+    const textElem = document.createElement("span");
+    textElem.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;";
+    frag.append(elem, textElem);
+  }
+  return frag;
+}
